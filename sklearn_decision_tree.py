848e7c2d3e0dff822c403dde5fdc32a6c48b1892
xAspro <sooryabalan.m@tifr.res.in>
2026-03-04 22:16:41 +0530

    Modified sklearn_decision_tree.py to select simplest model among the top models

--- sklearn_decision_tree.py
+++ sklearn_decision_tree.py
@@ -17,7 +17,6 @@ data = df.values
 X = data[:, :-1]
 y = data[:, -1].astype(int)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 tree = DecisionTreeClassifier(criterion='gini')
 param_grid = {
     'max_depth': [3, 5, 7, None],
@@ -35,7 +34,7 @@ grid_search = GridSearchCV(
     return_train_score=True
 )
 
-grid_search.fit(X_train, y_train)
+grid_search.fit(X, y)
 print(f"Best parameters: {grid_search.best_params_}")
 print(f"Best cross-validation accuracy: {grid_search.best_score_:.4f}")
 
@@ -51,35 +50,51 @@ print(results[[
     "mean_test_score",
     "std_test_score",
     "rank_test_score"
-]].head(5))
+]].head(15))
 
 
+def simple_model(results, max_tol=0.01):
+    best_std = results["std_test_score"].min()
+    print(f"\nBest std_test_score: {best_std:.4f}")
+    tol = max(max_tol, best_std)
 
-# Using the second best parameter because of its low std and same test score
-second_best_params = results.iloc[1]["params"]
+    top_score = results["mean_test_score"].max()
+    top_models = results[results["mean_test_score"] >= top_score - tol].copy()
 
-print("\nChoosing Second best params:", second_best_params)
+    top_models.loc[:, "depth_val"] = top_models["params"].apply(lambda x: float('inf') if x["max_depth"] is None else x["max_depth"])
+    top_models.loc[:, "leaf_val"] = top_models["params"].apply(lambda x: x["min_samples_leaf"])
 
-# Train new model using them
-second_model = DecisionTreeClassifier(
-    random_state=42,
-    **second_best_params
-)
+    simple_models = top_models.sort_values(["depth_val","leaf_val","mean_test_score"], ascending=[True, False, False])
+    simplest_model_params = simple_models.iloc[0]["params"]
+    print("\nTop models within tolerance:")
+    print(simple_models[[
+        "params",
+        "mean_train_score",
+        "mean_test_score",
+        "std_test_score",
+    ]])
 
-second_model.fit(X_train, y_train)
+    return simplest_model_params
+
+# Using the simplest model from the grid search results to train the final decision tree
+simplest_model_params = simple_model(results)
+print("\nChoosing simplest model params:", simplest_model_params)
+final_model = DecisionTreeClassifier(**simplest_model_params)
+final_model.fit(X, y)
+
 
 # # Uncomment to see the tree rules and the tree plot
-# tree_rules = export_text(second_model, feature_names=feature_names, show_weights=True)
-# print("\nSecond best tree rules:")
+# tree_rules = export_text(final_model, feature_names=feature_names, show_weights=True)
+# print("\nFinal model tree rules:")
 # print(tree_rules)
 
 # plt.figure(figsize=(12,8), dpi=300)
-# plot_tree(second_model, feature_names=feature_names, class_names=True, filled=True)
+# plot_tree(final_model, feature_names=feature_names, class_names=True, filled=True)
 # plt.savefig("tree.pdf")
 # plt.show()
 
 
-tree = second_model.tree_
+tree = final_model.tree_
 N = tree.n_node_samples[0]
 
 is_leaf = tree.children_left == -1
@@ -88,6 +103,7 @@ final_gini = np.sum(tree.impurity[is_leaf] * tree.n_node_samples[is_leaf]) / N
 print(f"Final Gini impurity of the tree: {final_gini:.4f}")
 print()
 
-test_predictions = second_model.predict(X_test)
-test_accuracy = accuracy_score(y_test, test_predictions)
-print(f"Test set accuracy with second-best parameters: {test_accuracy:.4f}\n")
+cross_val_scores = cross_val_score(final_model, X, y, cv=10, scoring='accuracy')
+print(f"Cross-validation scores: {cross_val_scores}")
+print(f"Mean cross-validation accuracy: {cross_val_scores.mean():.4f}")
+print(f"Standard deviation of cross-validation accuracy: {cross_val_scores.std():.4f}")
